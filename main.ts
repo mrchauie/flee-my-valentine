5879dad3ed32a046c0fc657e76406e6eea9fe2e5
mrchauie <39475891+mrchauie@users.noreply.github.com>
2022-11-22 07:58:55 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+// When your Player (grey heart) overlaps with a Cupid (red heart), the Cupid disappears and your Player heart grows
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Valentine, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scaling.scaleByPixels(sprite, 28, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+})
+// When your Player (grey heart) overlaps with an Arrow, the Arrow disappears and your Player heart shrinks
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Arrow, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scaling.scaleByPixels(sprite, -10, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+})
+// Every 1.8 seconds a red heart appears on the screen shooting this number of arrows
+game.onUpdateInterval(1800, function () {
+    valentine.check_win_or_lose()
+    valentine.send_valentine(assets.image`cupid hearts`, 4, assets.image`enemy`)
+})
